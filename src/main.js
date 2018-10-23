50b9016caaf11d4aa14a9b5387bc0513fccbe841
namaozi <shunya_ariga@dwango.co.jp>
2018-10-23 20:42:54 +0900

    ログイン機能実装

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyByjqg3qsTR6Z6rHeg9V5cFwjq1m8thiTc",
+  authDomain: "namaozi-markdown-id.firebaseapp.com",
+  databaseURL: "https://namaozi-markdown-id.firebaseio.com",
+  projectId: "namaozi-markdown-id",
+  storageBucket: "namaozi-markdown-id.appspot.com",
+  messagingSenderId: "284598629230"
+};
+firebase.initializeApp(config);
 
 new Vue({
   el: '#app',
