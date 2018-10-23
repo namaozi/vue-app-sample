6d9e79f27b1eae30a7f6251e9f176b73eeaac8c5
namaozi <shunya_ariga@dwango.co.jp>
2018-10-23 21:23:08 +0900

    ログインによるデータ出し分けとローディング表示

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <Home v-if="!isLogin"></Home>
-    <Editor v-if="isLogin"></Editor>
+    <Editor v-if="isLogin" :user="userData"></Editor>
+    <Loading v-if="!isLogin"></Loading>
   </div>
 </template>
 
 <script>
   import Home from './components/Home.vue';
   import Editor from './components/Editor.vue';
-
+  import Loading from './components/Loading.vue';
   import firebase from 'firebase';
 
   export default {
@@ -16,19 +17,21 @@
     data() {
       return {
         isLogin: false,
+        userData: null,
       }
     },
     created: function () {
       firebase
         .auth()
         .onAuthStateChanged(user => {
-           console.log(user);
-           this.isLogin = !!user; // truthy
+          this.isLogin = !!user; // truthy
+          this.userData = user || null;
         });
     },
     components: {
       Home: Home,
       Editor: Editor,
+      Loading: Loading,
     }
   }
 </script>
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="editor">
-    <h1>editor</h1>
+    <h1>エディタ</h1>
+    <span>{{ user.displayName }}</span>
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase';
   export default {
     name: 'editor',
+    props: [
+      'user',
+    ],
     data() {
       return {};
+    },
+    methods: {
+      logout: function () {
+        firebase.auth().signOut();
+      }
     }
   };
 </script>
