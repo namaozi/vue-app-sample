8e4986611d031cea360f603f9f6735695406ff02
namaozi <shunya_ariga@dwango.co.jp>
2018-10-23 19:26:08 +0900

    ログインモック

---
+++ src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="editor">
+    <h1>editor</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'editor',
+    data() {
+      return {};
+    }
+  };
+</script>
